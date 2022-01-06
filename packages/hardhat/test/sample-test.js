3e94773dfdf1de84ae8906b9259a1f8635225cf0
Sahil Vasava <sahilvasava@gmail.com>
2022-01-06 22:34:29 +0530

    cleaned up contract and test

--- packages/hardhat/test/sample-test.js
+++ packages/hardhat/test/sample-test.js
@@ -1,19 +1,10 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+describe("Dev3News", function () {
+  it("Should deploy Dev3News", async function () {
+    const Dev3NewsContract = await ethers.getContractFactory("Dev3News");
+    const dev3News = await Dev3NewsContract.deploy();
+    await dev3News.deployed();
   });
 });
